c654dba5707869d7c4d19e18c6c9de621a04aa15
Lucas Katz <l.katz92@gmail.com>
2023-10-31 15:13:12 -0300

    navbar con map

---
+++ components/button.js
@@ -0,0 +1,15 @@
+"use client"
+
+const Button = ({children, className = '', ...args}) => {
+
+    return (
+        <button
+            className={`btn-nav ${className}`}
+            {...args}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default Button
--- components/header.js
+++ components/header.js
@@ -1,8 +1,29 @@
 "use client"
 
 import { useState } from 'react';
+import { FaUser, FaShoppingCart } from 'react-icons/fa';
 import Link from 'next/link';
 import Image from 'next/image';
+import Button from './button';
+
+const links = [ 
+  {
+    label:"Home",
+    href:"/"
+  },
+  {
+    label:"Products",
+    href:"/products"
+  },
+  {
+    label:"About Us",
+    href:"/about"
+  },
+  {
+    label:"Contact",
+    href:"/contact"
+  }
+]
 
 
 export default function Navbar() {
@@ -14,56 +35,37 @@ export default function Navbar() {
 
   return (
     <nav className="flex items-center justify-between p-4 w-full text-purple-900">
-    <div className="flex items-center gap-2">
-      <Link href="#">
-        <Image
-          src="/logo.webp"
-          alt="Logo"
-          width={100}
-          height={40}
-        />
-      </Link>
-    </div>
-    <div className="hidden md:flex items-center justify-center flex-1">
-  <Link href="/">
-    <button className="nav-link mr-4">Home</button>
-  </Link>
-  <button onClick={toggleProducts} className="nav-link mr-4">
-    <Link href="products">Products </Link>
-  </button>
-  {isProductsOpen && (
-    <div className="absolute top-full left-0 mt-2 space-y-2 bg-white border border-gray-300 rounded-md w-48">
-      <Link href="/category1">
-        <button className="nav-link mr-4">Cat 1</button>
-      </Link>
-      <Link href="/category2">
-        <button className="nav-link mr-4">Cat 2</button>
-      </Link>
-      <Link href="/category3">
-        <button className="nav-link mr-4">Cat 3</button>
-      </Link>
-    </div>
-  )}
-  <Link href="/about">
-    <button className="nav-link mr-4">About Us</button>
-  </Link>
-  <Link href="/contact">
-    <button className="nav-link">Contact</button>
-  </Link>
-</div>
 
-    <div className="flex items-center justify-end space-x-4">
-      <Link href="/cart">
-        <button className="nav-link">Cart</button>
-      </Link>
-      <Link href="/login">
-        <button className="nav-link">Login</button>
-      </Link>
-      <Link href="/signup">
-        <button className="nav-link">Signup</button>
-      </Link>
-    </div>
-  </nav>
-  
-  );
+  <div className="flex items-center gap-2 p-4 w-1/4">
+    <Link href="#">
+      <Image
+        src="/logo.webp"
+        alt="Logo"
+        width={100}
+        height={40}
+      />
+    </Link>
+  </div>
+
+  <div className="hidden md:flex items-center justify-center flex-1 w-1/2">
+{
+links.map(link => (
+  <Link key={link.label} href={link.href} className="btn-nav">
+    {link.label}
+  </Link>
+))
+}
+  </div>
+
+  <div className="flex items-center justify-end space-x-4 p-4 w-1/4">
+    <Link href="/cart">
+     <FaShoppingCart className='text-3xl'/>
+    </Link>
+    <Link href="/login">
+    <FaUser className='text-3xl'/>
+    </Link>
+  </div>
+</nav>
+
+  )  
 }
