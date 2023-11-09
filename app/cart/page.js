e7e1eab4ba45a9db1b75ee60c5e5ff14efef2c8f
Lucas Katz <l.katz92@gmail.com>
2023-11-09 16:35:51 -0300

    cart faltan estilos

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,8 +1,26 @@
-export default function Login() {
+"use client"
+
+import Button from "@/components/userint/button";
+import Link from "next/link";
+
+
+
+export default function Cart() {
+
 
     return (
-      <main className="flex min-h-screen flex-col items-center justify-between ">
-        <h1>Aqui se renderizan los productos dentro del Cart</h1>
-      </main>
-    )
-  }
+        <>
+            <main className="container m-auto">
+                <div className="m-auto bg-orange-300 text-center">
+                <h1 className="m-auto py-12 text-2xl font-semibold text-purple-900">There are no products in your cart</h1>
+                <h2 className="m-auto py-12 text-2xl font-semibold text-purple-900">Click the button below to check our catalogue</h2>
+                    <Link href="/products/todos">
+                      <Button>
+                        Check Catalogue
+                      </Button>
+                    </Link>
+                </div>
+            </main>
+        </>
+    );
+}
