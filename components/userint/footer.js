56b61ad1833500876add89ee2a8b49e690fb4bf8
Lucas Katz <l.katz92@gmail.com>
2023-12-21 19:42:07 -0300

    Arregle detalle en footer

--- components/userint/footer.js
+++ components/userint/footer.js
@@ -8,13 +8,13 @@ const Footer = () => {
       <div className="container mx-auto py-3 w-full flex flex-col md:flex-row justify-between">
 
         {/* Left Section */}
-        <div className="w-full md:w-1/3 p-4 md:p-0 "> {/* Añade la propiedad border */}
+        <div className="w-full md:w-1/3 p-4"> {/* Añade la propiedad border */}
           <Link href={"/about"}>
             <h3 className="text-2xl font-semibold text-center font-lobster">About Us</h3>
-          </Link>
           <p className="mt-4 p-4">
             In the heart of the educational landscape, an innovative venture emerged from the passion of an English teacher. Fueled by the vision to redefine teaching, this entrepreneur set out to create a haven for educators.
           </p>
+          </Link>
         </div>
 
         {/* Middle Section */}
