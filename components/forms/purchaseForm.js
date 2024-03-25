b00c90c6a5d2f3a25e3a3acdfa44a762a7cd84f4
Lucas Katz <l.katz92@gmail.com>
2024-03-25 16:03:56 -0300

    Agregue generateEmailContent

--- components/forms/purchaseForm.js
+++ components/forms/purchaseForm.js
@@ -218,6 +218,28 @@ const PurchaseForm = () => {
         }
     };
 
+    const generateEmailContent = (userData, cart, orderId) => {
+        let emailContent = `
+          Night Owl Resources New Purchase
+          User: ${userData.name} ${userData.surname}
+          Email: ${userData.email}
+          Phone: ${userData.phone}
+          Purchase Details:
+        `;
+        cart.forEach((cartProduct) => {
+          emailContent += `
+            ${cartProduct.title} - Quantity: ${cartProduct.quantity}
+            Unit Price: $${cartProduct.price}
+          `;
+        });
+        emailContent += `
+          Total: $${cart.reduce((total, prod) => total + prod.price * prod.quantity, 0)}
+          Ticket ID: ${orderId}
+          Thank you for your purchase!`;
+        return emailContent;
+      };
+      
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
