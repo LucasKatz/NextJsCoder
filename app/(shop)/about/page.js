8ebb9b03c63927f2c0663039c61a61b5f5ac145c
Lucas Katz <l.katz92@gmail.com>
2024-03-26 09:00:35 -0300

    trabajo sobre instrucciones de compra

--- app/(shop)/about/page.js
+++ app/(shop)/about/page.js
@@ -14,20 +14,30 @@ export default function About() {
       <h1 className="text-center  my-7 text-text-color-5 font-extrabold text-2xl">Instrucciones de Compra</h1>
       <section className="flex flex-col md:flex-row items-center justify-between min-h-screen">
 
-        <div className="flex w-full md:w-1/2 mb-5 md:mr-5 ">
-          <Image
-            alt={""}
-            src={`/images/Lucy's%20new%20pencil-case.jpg`}
-            width={"660"}
-            height={"1240"}
-            className="mx-2 p-8"
-          />
+        <div className="flex w-full  mb-5 md:mr-5 ">
+            <p>Primero necesita loguearse para poder comprar. Encontrará el icono de ingreso/registro en la   esquina superior derecha de la pantalla. En caso de no contar con una cuenta, ingrese en la sección de registro y proceda a registrarse </p>
         </div>
 
-        <div className="flex w-full md:w-1/2 text-text-color-5 font-lobster text-xl text-center p-3 ">
-          <p>
-            In the heart of the educational landscape, an innovative venture emerged from the passion of an English teacher. Fueled by the vision to redefine teaching, this entrepreneur set out to create a haven for educators. Recognizing the need for high-quality, tailored teaching resources, the venture was born, aiming to empower teachers in making every lesson extraordinary. The founder&apos;s journey, inspired by personal classroom experiences, resulted in a collection of thoughtfully designed materials dedicated to simplifying lesson planning and enriching the educational experience. Today, this entrepreneurial endeavor stands as a testament to the transformative power of education, offering support to teachers worldwide as they strive to create impactful and memorable learning adventures.
-          </p>
+        <div className="flex w-full  text-text-color-5 font-lobster text-xl text-center p-3 ">
+            <p>Una vez ingresado, deberia ver su dirección de mail a la izquierda del botón de registro de la página. Desde el mismo, podra acceder a su perfil y/o cerrar sesión.</p>
+        </div>
+
+        <div className="flex w-full  text-text-color-5 font-lobster text-xl text-center p-3 ">
+            <p>Luego de loguearse, ir al catalogo que se encuentra en la sección de productos (se pueden ver las distitnas secciones en la barra de navegación en la parte superior). El mismo cuenta con un submenu para filtrar por categorias y/o idiomas</p>
+        </div>
+
+        <div className="flex w-full  text-text-color-5 font-lobster text-xl text-center p-3 ">
+            <p>Cada producto cuenta con dos botones. El boton de "agregar" añade el producto al carrito. El botón de "Ver Detalle" permite ver una breve descripción y otras caracteristicas del producto.</p>
+        </div>
+
+        <div className="flex w-full  text-text-color-5 font-lobster text-xl text-center p-3 ">
+            <p>Luego de agregar los productos que desea comprar, puede seleccionar la opción "ir al carrito" desde el botoón que se encuentra en la vista de "ver detalle" del producto o acceder al mismo desde el icono del carrito que se encuentra en la esquina superior derecha.</p>
+        </div>
+
+        <div className="flex w-full  text-text-color-5 font-lobster text-xl text-center p-3 ">
+            <p>Una vez verificado el detalle de su compra, clickee el boton "Proceder al pago". Seleccione el metodo de pago y responda a la pregunta de si desea descargar el ticket de compra.
+              Una vez finalizada la compra aparecerá un mensaje de agradecimiento y nos pondremos en contacto con usted cuando su pedido este listo.
+            </p>
         </div>
       </section>
     </main>
