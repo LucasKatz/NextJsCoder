a2f818af9fc8464c29805f846d939b707a575499
Lucas Katz <l.katz92@gmail.com>
2024-03-27 17:14:05 -0300

    casi terminado instrucciones

--- app/(shop)/about/page.js
+++ app/(shop)/about/page.js
@@ -19,33 +19,56 @@ export default function About() {
         <p>Primero necesita loguearse para poder comprar. Encontrará el icono de ingreso/registro en la esquina superior derecha de la pantalla. En caso de no contar con una cuenta, ingrese en la sección de registro y proceda a registrarse</p>
     </div>
     <div class="absolute left-28 top-8 h-12 w-12 bg-bg-color-3 flex justify-center items-center rounded-full text-white">
-        <span>1</span>
+        <span className="font-bold">1</span>
     </div>
 </div>
 
 
+<div class="relative">
+    <div class="flex text-text-color-5 bg-bg-color-3 font-lobster text-xl text-center p-3  w-3/4 ml-52 rounded-lg mb-5">
+        <p>Una vez ingresado, debería ver su dirección de correo electrónico a la izquierda del botón de registro de la página. Desde el mismo, podrá acceder a su perfil y/o cerrar sesión.</p>
+    </div>
+    <div class="absolute left-44 top-4 h-12 w-12 bg-bg-color-5 flex justify-center items-center rounded-full text-color-3">
+        <span className="text-red-600 font-bold">2</span>
+    </div>
+</div>
 
-        <div className="flex  text-text-color-5 bg-bg-color-3 font-lobster text-xl text-center p-3  w-3/4 ml-8 rounded-lg mb-5">
-            <p>Una vez ingresado, deberia ver su dirección de mail a la izquierda del botón de registro de la página. Desde el mismo, podra acceder a su perfil y/o cerrar sesión.</p>
-        </div>
+<div class="relative">
+    <div class="flex text-text-color-3 bg-bg-color-5 font-lobster text-xl text-center p-3 w-3/4 ml-36 rounded-lg mb-5">
+        <p>Luego de loguearse, ir al catálogo que se encuentra en la sección de productos (se pueden ver las distintas secciones en la barra de navegación en la parte superior). El mismo cuenta con un submenu para filtrar por categorías y/o idiomas.</p>
+    </div>
+    <div class="absolute left-28 top-8 h-12 w-12 bg-bg-color-3 flex justify-center items-center rounded-full text-white">
+        <span>3</span>
+    </div>
+</div>
 
-        <div className="flex bg-bg-color-5  text-text-color-3 font-lobster text-xl text-center p-3  w-3/4 mr-8 rounded-lg mb-5">
-            <p>Luego de loguearse, ir al catalogo que se encuentra en la sección de productos (se pueden ver las distitnas secciones en la barra de navegación en la parte superior). El mismo cuenta con un submenu para filtrar por categorias y/o idiomas</p>
-        </div>
+<div class="relative">
+    <div class="flex text-text-color-5 bg-bg-color-3 font-lobster text-xl text-center p-3  w-3/4 ml-52 rounded-lg mb-5">
+        <p>Cada producto cuenta con 2 botones. El botón de "agregar" agrega el producto al carrito. El botón de "ver detalle" permite ver una breve descripción y otras caracteristicas del producto.</p>
+    </div>
+    <div class="absolute left-44 top-4 h-12 w-12 bg-bg-color-5 flex justify-center items-center rounded-full text-color-3">
+        <span className="text-red-600 font-bold">4</span>
+    </div>
+</div>
 
-        <div className="flex   text-text-color-5 bg-bg-color-3 font-lobster text-xl text-center p-3  w-3/4 ml-8 rounded-lg mb-5">
-            <p>Cada producto cuenta con dos botones. El boton de "agregar" añade el producto al carrito. El botón de "Ver Detalle" permite ver una breve descripción y otras caracteristicas del producto.</p>
-        </div>
+        <div class="relative">
+    <div class="flex text-text-color-3 bg-bg-color-5 font-lobster text-xl text-center p-3 w-3/4 ml-36 rounded-lg mb-5">
+        <p>Luego de agregar los productos que desea comprar, puede seleccionar la opción "ir al carrito" desde el botón que se encuentra en la vista de "ver detalle" del producto o acceder al mismo desde el icono del carrito que se encuentra en la esquina superior derecha.</p>
+    </div>
+    <div class="absolute left-28 top-8 h-12 w-12 bg-bg-color-3 flex justify-center items-center rounded-full text-white">
+        <span>5</span>
+    </div>
+</div>
 
-        <div className="flex   bg-bg-color-5  text-text-color-3 font-lobster text-xl text-center p-3  w-3/4 mr-8 rounded-lg mb-5">
-            <p>Luego de agregar los productos que desea comprar, puede seleccionar la opción "ir al carrito" desde el botoón que se encuentra en la vista de "ver detalle" del producto o acceder al mismo desde el icono del carrito que se encuentra en la esquina superior derecha.</p>
-        </div>
-
-        <div className="flex   text-text-color-5 bg-bg-color-3 font-lobster text-xl text-center p-3 mb-5  w-3/4 ml-8 rounded-lg mb-5">
-            <p>Una vez verificado el detalle de su compra, clickee el boton "Proceder al pago". Seleccione el metodo de pago y seleccione si desea descargar el ticket de compra.
-              Una vez finalizada la compra aparecerá un mensaje de agradecimiento y nos pondremos en contacto con usted cuando su pedido este listo.
-            </p>
-        </div>
+<div class="relative">
+    <div class="flex text-text-color-5 bg-bg-color-3 font-lobster text-xl text-center p-3  w-3/4 ml-52 rounded-lg mb-5">
+        <p>Una vez verificado el detalle de su compra, clickee el boton "Proceder al pago". Seleccione el metodo de pago y seleccione si desea descargar el ticket de compra.
+              Una vez finalizada la compra aparecerá un mensaje de agradecimiento y nos pondremos en contacto con usted cuando su pedido este listo.</p>
+    </div>
+    <div class="absolute left-44 top-8 h-12 w-12 bg-bg-color-5 flex justify-center items-center rounded-full text-color-3">
+        <span className="text-red-600 font-bold">6</span>
+    </div>
+</div>
       </section>
     </main>
   );
