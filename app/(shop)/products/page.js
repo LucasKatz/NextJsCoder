e8867dc9ebc18e7353117a0fbcab434075ba79bf
Lucas Katz <l.katz92@gmail.com>
2024-04-23 23:02:50 -0300

    pictures again

--- app/(shop)/products/page.js
+++ app/(shop)/products/page.js
@@ -11,15 +11,17 @@ export default function Language() {
 
 
                 <div className="flex flex-col items-center mx-auto">
-                    <div className="mx-auto bg-white h-64 w-64" style={{ backgroundSize: 'cover'}}>
-                    </div>
+                <div className="mx-auto bg-white h-64 w-64" style={{ backgroundSize: 'cover', backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/night-owl-resources-757d9.appspot.com/o/Hello%20World.webp?alt=media&token=a11e01e2-d690-4f22-aa55-cbd106cec6f8)'}}>
+</div>
+
                     <Link href={"/productos/english"}>
                         <button className="text-text-color-5 bg-bg-color-3 font-lobster text-xl text-center mt-4 p-3 rounded-md">English</button>
                     </Link>
                 </div>
                 <div className="flex flex-col items-center mx-auto">
-                    <div className="mx-auto bg-white h-64 w-64" style={{ backgroundSize: 'cover', backgroundPosition:"center"}}>
-                    </div>
+                <div className="mx-auto bg-white h-64 w-64" style={{ backgroundSize: 'cover', backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/night-owl-resources-757d9.appspot.com/o/Hola-Mundo.webp?alt=media&token=7e34d9fc-6101-42db-94d6-b209609a176a)', backgroundPosition: "center"}}>
+</div>
+
                     <Link href={"/productos/spanish"}>
                         <button className="text-text-color-5 bg-bg-color-3 font-lobster text-xl text-center mt-4 p-3 rounded-md">Español</button>
                     </Link>
